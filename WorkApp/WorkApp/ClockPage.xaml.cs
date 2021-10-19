977185680c77714aaa65cffb979df91c1f274600
SmolyaninaLA <SmolyaninaLA@arkona36.ru>
2021-10-19 20:17:39 +0300

    Итоговое к модулю 39 Xamarin

--- WorkApp/WorkApp/ClockPage.xaml.cs
+++ WorkApp/WorkApp/ClockPage.xaml.cs
@@ -16,115 +16,81 @@ namespace WorkApp
         public ClockPage()
         {
             InitializeComponent();
-            AlarmClock();
-        }
-
-        public void AlarmClock()
-        {
            
-
-            var datePicker = new DatePicker
-            {
-                Format = "D",
-
-                MaximumDate = DateTime.Now.AddDays(7),
-                MinimumDate = DateTime.Now.AddDays(-7),
-            };
-
-            var datePickerText = new Label { Text = "Дата запуска ", Margin = new Thickness(40, 20, 0, 0) };
-
-            stackLayout.Children.Add(datePickerText);
-            stackLayout.Children.Add(datePicker);
-            
-            var timePickerText = new Label { Text = "Время запуска ", Margin = new Thickness(40, 20, 0, 0) };
-            var timePicker = new TimePicker
-            {
-                Time = new TimeSpan(13, 0, 0)
-            };
-
-            stackLayout.Children.Add(timePickerText);
-            stackLayout.Children.Add(timePicker);
-
-            Slider slider = new Slider
-            {
-                Minimum = 0,
-                Maximum = 50,
-                Value = 15.0,
-                ThumbColor = Color.DodgerBlue,
-                MinimumTrackColor = Color.DodgerBlue,
-                MaximumTrackColor = Color.Gray
-            };
-
-            var sliderText = new Label { Text = $"Громкость сигнала: {slider.Value} ", HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(40, 30, 0, 0) };
-
-            stackLayout.Children.Add(sliderText);
-            stackLayout.Children.Add(slider);
-
-            var switchHeader = new Label { Text = "Каждый день", HorizontalOptions = LayoutOptions.Center, Margin = new Thickness(40, 5, 0, 0) };
-            stackLayout.Children.Add(switchHeader);
-
-            Switch switchControl = new Switch
-            {
-                IsToggled = false,
-                HorizontalOptions = LayoutOptions.Center,
-                ThumbColor = Color.DodgerBlue,
-                OnColor = Color.LightSteelBlue,
-            };
-            stackLayout.Children.Add(switchControl);
-
-            Button buttonSave = new Button
-            {
-                Text = "Сохранить",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
-                BorderWidth = 1,
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.CenterAndExpand
-            };
-
-            stackLayout.Children.Add(buttonSave);
-
-            Button buttonReturn = new Button
-            {
-                Text = "На главную",
-                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Button)),
-                BorderWidth = 1,
-                HorizontalOptions = LayoutOptions.Center,
-                VerticalOptions = LayoutOptions.EndAndExpand
-            };
-
-            stackLayout.Children.Add(buttonReturn);
-
-            timePicker.PropertyChanged += (sender, e) => TimeChangedHandler(sender, e, timePickerText, timePicker);
-            datePicker.DateSelected += (sender, e) => DateSelectedHandler(sender, e, datePickerText);
-            slider.ValueChanged += (sender, e) => VolumeChangedHandler(sender, e, sliderText);
-            buttonSave.Clicked += (sender, e) =>  Save_Click(sender,e,datePickerText,timePickerText);
-            buttonReturn.Clicked += Return_Click;
-
         }
-
-        public void DateSelectedHandler(object sender, DateChangedEventArgs e, Label datePickerText)
+       
+        public void DateSelectedHandler(object sender, DateChangedEventArgs e)
         {
-           
+           if (e.NewDate >= DateTime.Today )
+            {
+                VisualStateManager.GoToState(datePicker, "Valid");
+            }
+           else
+            {
+                VisualStateManager.GoToState(datePicker, "Invalid");
+            }
             datePickerText.Text = "Дата запуска " + e.NewDate.ToString("dd/MM/yyyy");
         }
 
-        public void TimeChangedHandler(object sender, PropertyChangedEventArgs e, Label timePickerText, TimePicker timePicker)
+        
+        public void TimeChangedHandler(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
-            
+
             if (e.PropertyName == "Time")
+            {
+                if (datePicker.Date > DateTime.Today)
+                {
+                    VisualStateManager.GoToState(timePicker, "Valid");
+                }
+                else
+                    if (datePicker.Date == DateTime.Today)
+                    {
+                        DateTime dt = datePicker.Date + timePicker.Time;
+                        var comp = dt.CompareTo(DateTime.Now);
+                        if (comp < 0)
+                        {
+                            VisualStateManager.GoToState(timePicker, "Invalid");
+                        }
+                        else
+                        {
+                            VisualStateManager.GoToState(timePicker, "Valid");
+                        }
+
+                    }
+                    else
+                    {
+                        VisualStateManager.GoToState(timePicker, "Invalid");
+                    }
                 timePickerText.Text = "Время запуска " + timePicker.Time;
+            }
+                
+
         }
 
-        public void VolumeChangedHandler(object sender, ValueChangedEventArgs e, Label header)
+        
+        public void VolumeChangedHandler(object sender, ValueChangedEventArgs e )
         {
-            header.Text = String.Format("Громкость: {0}", e.NewValue);
+           
+            sliderText.Text = string.Format("Громкость: {0}", e.NewValue);
         }
 
-        public void Save_Click(object sender, EventArgs e, Label dt, Label tm)
+        
+        public void Save_Click(object sender, EventArgs e)
         {
-            var str = "Будильник установлен : " + dt.Text + " , " + tm.Text;
+           
+            var str = "Будильник установлен : " + datePickerText.Text + " , " + timePickerText.Text;
             DisplayAlert("Внимание", str , "OK");
+           
+            var controls = new View[] { datePicker, timePicker , sliderVolume , switchEveryDay };
+            ButtonChangeProperty(controls);
+
         }
+
+        private void ButtonChangeProperty( View[] views)
+        {
+            foreach (var view in views)
+                view.IsEnabled = false;
+        } 
 
         private async void Return_Click(object sender, EventArgs e)
         {
